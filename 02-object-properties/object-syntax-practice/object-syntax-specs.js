924c7dc74bd7b8a6e433d1736f4bc2bc20180544
Scott D'Alessandro <sdaless22@gmail.com>
2018-08-07 09:36:44 -0400

    Update object-syntax-specs.js

--- 02-object-properties/object-syntax-practice/object-syntax-specs.js
+++ 02-object-properties/object-syntax-practice/object-syntax-specs.js
@@ -6,12 +6,17 @@ describe('shortand object syntax', function() {
     expect(typeof dog.bark).toBe('function');
     expect(dog.bark.toString().includes('function')).toBe(false);
   });
-  it('uses the global name variable as and assigns it as a property using shorthand-property syntax', function() {
+  it('uses the global name variable and assigns it as a property using shorthand-property syntax', function() {
     expect(dog.name).toBeDefined();
     expect(name).toBeDefined();
     expect(dog.name).toBe('Cody');
   });
-  it('uses a function to create the computed property "age"', function() {
+  it('creates a bark method on dog', () => {
+    expect(dog.bark).toBeDefined();
+    expect(typeof dog.bark).toBe('function');
+    expect(dog.bark()).toBe('ruff ruff');
+  });
+  it('uses the global variable loud to create the computed method loudBark', function() {
     expect(loud).toBeDefined();
     expect(loud).toBe('loud');
     expect(dog['loudBark']()).toBe('RUFF RUFF!');
@@ -23,3 +28,4 @@ describe('shortand object syntax', function() {
     expect(typeof loudBark).toBe('function');
   });
 });
+
