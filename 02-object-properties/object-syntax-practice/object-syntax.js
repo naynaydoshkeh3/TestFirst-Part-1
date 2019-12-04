8e62977f5be5a771c4fa174be6413a860d222ba1
michaelrios2015 <michael.rios093@gmail.com>
2019-12-03 23:04:39 -0500

    more work on part two

--- 02-object-properties/object-syntax-practice/object-syntax.js
+++ 02-object-properties/object-syntax-practice/object-syntax.js
@@ -1,9 +1,14 @@
 /* eslint-disable no-unused-vars */
 
+const loud = 'loud';
+
 const dog = {
-    topping: 'sausage',
-    size: 'large',
-    restaurant: 'Table87',
-    deliveryTime: '25 minutes',
-    price: 27.99
+  
+  name:  "Cody",
+
+  bark() {
+    return 'ruff ruff!';
+  }
+
   };
+
